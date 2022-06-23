dd0ae30aede92eedf7d251e62188b0fcdc7c4f8e
agirb <agirbasomer35@gmail.com>
2022-06-24 00:57:33 +0300

    cucumber_project

---
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinations.java
@@ -0,0 +1,22 @@
+package com.cydeo.step_definitions;
+
+import com.cydeo.utilities.Driver;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class GoogleStepDefinations {
+    @When("user ison Google sarch page")
+    public void user_ison_google_sarch_page() {
+        // Write code here that turns the phrase above into concrete actions
+        Driver.getDriver().get("https://www.google.com");
+    }
+    @Then("user should see title is Google")
+    public void user_should_see_title_is_google() {
+        // Write code here that turns the phrase above into concrete actions
+        String expectedTitle="Google";
+        String actualTitle=Driver.getDriver().getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
+        Driver.closeDriver();
+    }
+}
---
+++ src/test/resources/features/googleSearch.feature
@@ -0,0 +1,7 @@
+Feature: Google search functionality
+  Agile story : As a user , when I am on the Google search page
+  i hould be able to search whatever I want and see relevant information
+
+  Scenario:  Search page tiitle verification
+    When user ison Google sarch page
+    Then user should see title is Google
