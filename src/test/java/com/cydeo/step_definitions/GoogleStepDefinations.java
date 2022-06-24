817e37eb60cb2d8d0b87d764a055aae5ca28ecff
agirb <agirbasomer35@gmail.com>
2022-06-24 13:06:17 +0300

    day 16 googleSearch.feature

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinations.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinations.java
@@ -1,13 +1,14 @@
 package com.cydeo.step_definitions;
 
 import com.cydeo.utilities.Driver;
+
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 
 public class GoogleStepDefinations {
-    @When("user is on Google sarch page")
-    public void user_ison_google_sarch_page() {
+    @When("user is on Google search page")
+    public void user_is_on_google_sarch_page() {
         // Write code here that turns the phrase above into concrete actions
         Driver.getDriver().get("https://www.google.com");
     }
@@ -19,4 +20,6 @@ public class GoogleStepDefinations {
         Assert.assertEquals(actualTitle, expectedTitle);
         Driver.closeDriver();
     }
+
+
 }
